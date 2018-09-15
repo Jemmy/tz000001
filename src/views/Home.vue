8de463ec2f38e44e520a3a7dbb5b6ac90699a853
Jemmy <2070702@gmail.com>
2018-09-15 06:59:25 +0300

    Исправление стилей #nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="home">
-      <div id="nav">
-          <router-link to="/desktop">Интерактивный рабочий стол</router-link>
-          |
-          <router-link to="/blockchain">Взаимодействие с сервером по протоколу websocket</router-link>
-      </div>
+    <div id="nav">
+      <router-link to="/desktop">Интерактивный рабочий стол</router-link>
+      |
+      <router-link to="/blockchain.v1">Взаимодействие с сервером по протоколу websocket v1</router-link>
+      |
+      <router-link to="/blockchain.v2">Взаимодействие с сервером по протоколу websocket v2</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'home'
-}
+  export default {
+    name: 'home'
+  }
 </script>
+
+<style lang="scss" scoped>
+  #nav {
+    margin-top: 24px;
+    text-align: center;
+  }
+</style>
